8844b542ba0a07ef677fd333bed3a6af9259b69d
Maha-Mostafa <eng.maha.mostafa@gmail.com>
2017-12-27 16:10:34 +0200

    add get resource

--- src/main/java/org/bibalex/eol/scheduler/resource/ResourceController.java
+++ src/main/java/org/bibalex/eol/scheduler/resource/ResourceController.java
@@ -13,36 +13,42 @@ import java.util.concurrent.Callable;
 
 
 @RestController
-@RequestMapping("{contentPartnerId}/resources")
+@RequestMapping("/")
 public class ResourceController {
     private static final Logger logger = Logger.getLogger(ResourceController.class);
     @Autowired
     private ResourceService resourceService;
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, value = "{contentPartnerId}/resources")
     public Callable<ResponseEntity<?>> createResource(@PathVariable long contentPartnerId, @RequestBody Resource resource){
         logger.debug("Create new resource with partner id "+contentPartnerId);
         return () -> ResponseEntity.ok(resourceService.createResource(contentPartnerId, resource));
     }
-    @RequestMapping(method = RequestMethod.POST, value = "/{resourceId}")
+    @RequestMapping(method = RequestMethod.POST, value = "{contentPartnerId}/resources/{resourceId}")
     public Callable<ResponseEntity<Resource>> updateResource(@PathVariable long contentPartnerId, @PathVariable long resourceId, @RequestBody Resource resource){
         logger.debug("Update resource with id "+resourceId );
         return () ->  ResponseEntity.ok(resourceService.updateResource(contentPartnerId, resourceId, resource));
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{resourceId}")
+    @RequestMapping(method = RequestMethod.GET, value = "{contentPartnerId}/resources/{resourceId}")
     public Callable<ResponseEntity<Resource>> getResource(@PathVariable long resourceId){
         logger.debug("Get resource of id "+resourceId );
         return () ->  ResponseEntity.ok(resourceService.getResource(resourceId));
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, value = "{contentPartnerId}/resources")
     public ResponseEntity<Collection<Resource>> getResources(@PathVariable long contentPartnerId){
         List<Resource> resources = resourceService.getResources(contentPartnerId);
         HttpStatus status = HttpStatus.OK;
         if (resources == null)
             status = HttpStatus.NOT_FOUND;
         return new ResponseEntity<>(resources, status);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "resources/{resourceId}")
+    public Callable<ResponseEntity<Resource>> getResourceWithoutCP(@PathVariable long resourceId){
+        logger.debug("Get resource of id "+resourceId );
+        return () ->  ResponseEntity.ok(resourceService.getResource(resourceId));
     }
 
 }
